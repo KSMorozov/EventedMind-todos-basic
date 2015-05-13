d26bbd925b02a9cef313f08945df4aeb71c52919
Kirill Morozov <tiguhrrr@gmail.com>
2015-05-14 01:47:50 +0300

    Build a Simple Application Part 2

--- todos.js
+++ todos.js
@@ -3,7 +3,60 @@ Todos = new Meteor.Collection('todos');
 Meteor.isClient ? (function() {
   Template.todosPanel.helpers({
     items: function() {
-      return Todos.find();
+      return Todos.find({}, {
+        sort: {
+          created_at: -1
+        }
+      });
+    },
+
+    isDoneClass: function() {
+      return this.is_done ? 'done' : '';
+    }
+  });
+
+  Template.todoItem.helpers({
+    isDoneChecked: function() {
+      return this.is_done ? 'checked' : '';
+    }
+  });
+
+  Template.todosCount.helpers({
+    completedCount: function() {
+      return Todos.find({is_done: true}).count();
+    },
+
+    totalCount: function() {
+      return Todos.find().count();
+    }
+  });
+
+  Template.todoItem.events({
+    'click [name=is_done]': function(e, template) {
+      var id = this._id;
+      var isDone = template.find('input').checked;
+      Todos.update({_id: id}, {
+        $set: {
+          is_done: isDone
+        }
+      });
+    }
+  });
+
+  Template.createTodoItem.events({
+    'submit form': function(e, template) {
+      e.preventDefault();
+
+      var subject = template.find('input').value;
+
+      Todos.insert({
+        subject: subject,
+        created_at: new Date,
+        is_done: false
+      });
+
+      var form = template.find('form');
+      form.reset();
     }
   });
 }()) : (function() {
